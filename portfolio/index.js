b72a5b0807d6f2a51f2f51452cc15ac883f4b71f
Tatsiana Sapun <tatiana.sapun@gmail.com>
2022-01-14 18:49:06 +0300

    feat: add adaptive header and hero section

--- portfolio/index.js
+++ portfolio/index.js
@@ -38,3 +38,27 @@ console.log ('Ваша оценка - 110 баллов\n' +
 'их по своему усмотрению, руководствуясь общим стилем макета\n' +
 '29) обязательное требование к интерактивности: плавное изменение внешнего вида элемента при наведении и клике не\n' +
 'влияющее на соседние элементы\n');
+
+const hamburgerButton = document.querySelector('.hamburger');
+const nav = document.querySelector('.nav');
+const navList = document.querySelector('.nav-list');
+const navLinks = document.querySelectorAll('.nav-link');
+const blackout = document.createElement('div');
+document.body.append(blackout);
+
+hamburgerButton.addEventListener('click', function () {
+  hamburgerButton.classList.toggle('is-active');
+  nav.classList.toggle('is-active');
+  navList.classList.toggle('is-active');
+  blackout.classList.toggle('blackout');
+});
+
+function closeMenu() {
+  if (event.target.classList.contains('nav-link')) {
+    hamburgerButton.classList.remove('is-active');
+    nav.classList.remove('is-active');
+    navList.classList.remove('is-active');  
+    blackout.classList.toggle('blackout');
+  }
+}
+nav.addEventListener('click', closeMenu);
