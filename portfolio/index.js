ffa8714c47aa7a8e8613d73d8344cd30e6c6bd94
Tatsiana Sapun <tatiana.sapun@gmail.com>
2022-01-21 11:17:25 +0300

    feat: add page translation opportunity

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,9 +1,12 @@
 import { showSelfTest } from './scripts/self-test.js';
 import { initMenu } from './scripts/menu.js';
 import { changeSeason } from './scripts/portfolio.js';
+import { translate } from './scripts/translation.js';
 
 showSelfTest();
 
 initMenu();
 
 changeSeason();
+
+translate();
