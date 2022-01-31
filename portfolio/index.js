cee9026811c3e8be3ef084471027b9387e966abd
Tatsiana Sapun <tatiana.sapun@gmail.com>
2022-01-31 18:17:47 +0300

    feat: add play and mute functions

--- portfolio/index.js
+++ portfolio/index.js
@@ -5,6 +5,7 @@ import { preloadImages } from './scripts/portfolio.js';
 import { initTranslation } from './scripts/translation.js';
 import { initChangingTheme } from './scripts/theme.js';
 import { initButtonsEffects } from './scripts/buttons.js'
+import { video } from './scripts/video-player.js'
 
 showSelfTest();
 
@@ -17,6 +18,8 @@ initTranslation();
 initChangingTheme();
 
 initButtonsEffects();
+
+video();
 
 document.querySelector('body').style.display = 'flex';
 
---
+++ portfolio/scripts/video-player.js
@@ -0,0 +1,28 @@
+export function video() {
+  const video = document.querySelector('.video');
+  const playButtons = document.querySelectorAll('.play, .play-button');
+  const volumeStatus = document.querySelector('.volume-status');
+
+  let lastVolume = 1;
+
+
+  function playVideo() {
+    let method = (video.paused) ? 'play' : 'pause';
+    video[method]();
+    playButtons.forEach(button => button.classList.toggle('video-in-progress'));
+  }
+
+  playButtons.forEach(button => button.addEventListener('click', playVideo));
+
+  function changeVolume() {
+    if (video.volume) {
+      video.volume = 0;
+      volumeStatus.classList.add('muted');
+    } else {
+      video.volume = lastVolume;
+      volumeStatus.classList.remove('muted');
+    }
+  }
+
+  volumeStatus.addEventListener('click', changeVolume);
+}
