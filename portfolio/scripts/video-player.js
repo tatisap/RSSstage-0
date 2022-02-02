dee4f4fd2adf1af92483fadb40ad8ddb76a84510
Tatsiana Sapun <tatiana.sapun@gmail.com>
2022-02-02 19:21:25 +0300

    feat: add setting progress function

--- portfolio/scripts/video-player.js
+++ portfolio/scripts/video-player.js
@@ -7,13 +7,14 @@ export function video() {
   let lastVolume = 1;
 
 
-  function playVideo() {
+  function toggleVideo() {
     let method = (video.paused) ? 'play' : 'pause';
     video[method]();
     playButtons.forEach(button => button.classList.toggle('video-in-progress'));
   }
 
-  playButtons.forEach(button => button.addEventListener('click', playVideo));
+  video.addEventListener('click', toggleVideo);
+  playButtons.forEach(button => button.addEventListener('click', toggleVideo));
 
   function changeVolume() {
     if (video.volume) {
@@ -33,4 +34,28 @@ export function video() {
   }
 
   video.addEventListener('timeupdate', updateProgress);
+
+  function setProgress(event) {
+    if (event.target === progressBar) {
+      
+      video.removeEventListener('timeupdate', updateProgress);
+      document.addEventListener('mousemove', onMouseMove);
+      document.addEventListener('mouseup', onMouseUp);
+    }
+
+      function onMouseMove() {
+        video.currentTime =  progressBar.value * video.duration / 100;
+        progressBar.style.backgroundImage = `linear-gradient(to right, #bdae82 ${Math.round(progressBar.value)}%, #bdae82 0%, #b3b3bb 0%, #b3b3bb ${100 - (Math.round(progressBar.value))}%)`;
+      }
+
+      function onMouseUp() {
+        video.currentTime = progressBar.value * video.duration / 100;
+
+        video.addEventListener('timeupdate', updateProgress);
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
+      }
+  }
+
+  document.addEventListener('mousedown', setProgress);
 }
