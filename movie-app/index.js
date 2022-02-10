6e001a5c82555bd6f3fe1671c9aaa27252777645
Tatsiana Sapun <tatiana.sapun@gmail.com>
2022-02-10 13:21:48 +0300

    fix: change base url to secure base url

--- movie-app/index.js
+++ movie-app/index.js
@@ -6,7 +6,6 @@ async function getPopularMovies() {
   const popularMoviesUrl = 'https://api.themoviedb.org/3/movie/popular?api_key=fcbdf426eda6fb1641d20038dd9e99f5&language=en-US&page=1';
   const popularMoviesRes = await fetch(popularMoviesUrl);
   const popularMoviesData = await popularMoviesRes.json();
-  console.log(popularMoviesData);
 
   const configRes = await fetch('https://api.themoviedb.org/3/configuration?api_key=fcbdf426eda6fb1641d20038dd9e99f5');
   const configData = await configRes.json();
@@ -33,7 +32,6 @@ async function getDataByQuery(event) {
   const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=fcbdf426eda6fb1641d20038dd9e99f5&language=en-US&query=${query}&page=1&include_adult=false`;
   const foundMoviesRes = await fetch(searchUrl);
   const foundMoviesData = await foundMoviesRes.json();
-  console.log(foundMoviesData);
 
   const configRes = await fetch('https://api.themoviedb.org/3/configuration?api_key=fcbdf426eda6fb1641d20038dd9e99f5');
   const configData = await configRes.json();
@@ -64,7 +62,7 @@ function addPosters(data, config) {
     if ( data.results[i].poster_path === null ) {
       posters[i].style.display = 'none';
     } else {
-      posters[i].src = config.images.base_url + config.images.backdrop_sizes[0] + data.results[i].poster_path;
+      posters[i].src = config.images.secure_base_url + config.images.backdrop_sizes[0] + data.results[i].poster_path;
       posters[i].alt = data.results[i].title;
     }
   }
